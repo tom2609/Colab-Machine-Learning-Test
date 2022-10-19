fe5f571dd2cdfb7fb2a956ec33f02af69b580a8b
tom2609 <tomemmerich2609@gmail.com>
2022-10-19 14:02:06 +0200

    Start MNIST Number Recognition

--- MNIST Number recognition.py
+++ MNIST Number recognition.py
@@ -1,4 +1,18 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from matplotlib import pyplot as plt
+import idx2numpy
+import tensorflow as tf
+
+if tf.config.list_physical_devices('GPU'):
+  print("TensorFlow **IS** using the GPU")
+else:
+  print("TensorFlow **IS NOT** using the GPU")
+
+imagefile = r"C:\Users\Tom\Projects\Data Stash\train-images.idx3-ubyte"
+imagearray = idx2numpy.convert_from_file(imagefile)
+
+plt.imshow(imagearray[3], cmap=plt.cm.binary)
+plt.show()
+
+# print(imagearray[3])
